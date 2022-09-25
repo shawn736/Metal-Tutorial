4292ecf48e7b7cdf7bff6effd66b836522cec470
Shawn736 <xwh736@163.com>
2022-09-25 08:04:26 +0800

    update

--- Ch12-Playground.playground/Contents.swift
+++ Ch12-Playground.playground/Contents.swift
@@ -5,5 +5,6 @@ import PlaygroundSupport
 let device = MTLCreateSystemDefaultDevice()!
 let frame = NSRect(x: 0, y: 0, width: 400, height: 400)
 let view = MetalView(frame: frame, device: device)
+view.framebufferOnly = false
 PlaygroundPage.current.liveView = view
 
